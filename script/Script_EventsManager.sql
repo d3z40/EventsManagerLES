4e447604e37235837b1e953a5c5d9464e394ea1e
André de Andrade Pereira <andre.pereira22@fatec.sp.gov.br>
2018-09-04 05:49:15 -0300

    Inserindo script para BD

---
+++ script/Script_EventsManager.sql
@@ -0,0 +1,44 @@
+DROP DATABASE IF EXISTS LES;
+CREATE DATABASE LES;
+USE LES;
+
+CREATE TABLE tb_endereco (
+  id INT NOT NULL AUTO_INCREMENT,
+  logradouro VARCHAR(255) NOT NULL,
+  numero INT  NOT NULL,
+  bairro VARCHAR(255) NOT NULL,
+  cidade VARCHAR(255) NOT NULL,
+  estado VARCHAR(255) NOT NULL,
+  cep VARCHAR(8) NOT NULL,
+  dtCadastro DATE NOT NULL,
+  CONSTRAINT pk_endereco PRIMARY KEY (id)
+);
+
+CREATE TABLE tb_usuario (
+  id INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(255) NOT NULL,
+  apelido VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  senha VARCHAR(255) NOT NULL,
+  cpf VARCHAR(11) NOT NULL,
+  dtCadastro DATE NOT NULL,
+  CONSTRAINT pk_usuario PRIMARY KEY (id)
+);
+
+CREATE TABLE tb_usuario_endereco (
+	id INT NOT NULL AUTO_INCREMENT,
+    idUsuario INT NOT NULL,
+    idEndereco INT  NOT NULL,
+    CONSTRAINT pk_usuario_endereco PRIMARY KEY (id),
+    CONSTRAINT fk_usuario FOREIGN KEY (idUsuario) REFERENCES tb_usuario (id),
+	CONSTRAINT fk_endereco FOREIGN KEY (idEndereco) REFERENCES tb_endereco (id)
+);
+
+CREATE TABLE tb_amigo (
+	id INT NOT NULL AUTO_INCREMENT,
+    idUsuario INT NOT NULL,
+    idAmigo INT NOT NULL,
+    CONSTRAINT pk_amigo PRIMARY KEY (id),
+    CONSTRAINT fk_usuario_usuario FOREIGN KEY (idUsuario) REFERENCES tb_usuario (id),
+	CONSTRAINT fk_amigo_usuario FOREIGN KEY (idAmigo) REFERENCES tb_usuario (id)
+);
